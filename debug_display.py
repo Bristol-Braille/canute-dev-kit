2a2a5297ea788eb5a6f977a5c40cca36fc2b260c
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-09-29 01:04:54 +0100

    Make pipe seperator an option

--- debug_display.py
+++ debug_display.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import unicode_literals
-
 import serial
+import sys
+
+#./debug_display.py sep means seperate the output with a pipe character
+if sys.argv[-1][:3] == 'sep':
+    seperator = '|'
+else:
+    seperator = ''
+
 
 def pin_num_to_unicode(pin_num):
     return unichr(pin_num+10240)
@@ -13,11 +20,8 @@ with serial.Serial('/dev/ttyUSB0', 115200) as ser:
         evens = line[0:-1:2]
         odds = line[1:-1:2]
         output = ''
-        if line == '\n':
-            print('')
-        else:
-            for i,c in enumerate(evens):
-                if c != '\r':
-                    pin_num = int(c) | (int(odds[i]) << 3)
-                    output += '|{}'.format(pin_num_to_unicode(pin_num))
-            print(output)
+        for i,c in enumerate(evens):
+            if c != '\r':
+                pin_num = int(c) | (int(odds[i]) << 3)
+                output += seperator + pin_num_to_unicode(pin_num)
+        print(output)
